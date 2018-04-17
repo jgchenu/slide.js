5a82ed243945920459fecd1bfb78e1556f73e412
Deer <2016130201@email.szu.edu.cn>
2018-04-17 13:44:52 +0800

    增加了手指滑动功能，以及滑动灵敏度自定义，增加了移动端es5兼容性支持

--- slide.js
+++ slide.js
@@ -1,23 +1,25 @@
-
 (function () {
     let Slide = function (
         slideId = 'slide',
         timeOut = 3000,
-        speed = 0.1
+        speed = 0.1,
+        d = 20
     ) {
+        this.d = d;//滑动灵敏度
+        this.timeOut = timeOut;//轮播间隔
         this.slide = document.querySelector('#slide'); //获取视窗层
         this.room = this.slide.querySelector('div'); //获取内容层
         this.imgs = [...this.slide.querySelectorAll('.slide-item')]; //轮播图元素组
         this.slideWidth = parseInt(getComputedStyle(this.slide).width); //视窗层高度
         this.slideHeight = parseInt(getComputedStyle(this.slide).height); //视窗层宽度
-
+        this.startPoint = null; //手指触摸的起点
         this.navButtons = null; //获取到所有的导航圆点
         this.timer = null; //定时器
         this.imgIndex = 1; //当前轮播图片index
         this.prev = null; //上一个
         this.next = null; //下一个
         this.speed = -(this.slideWidth / 10 * speed); //动画速度
-        this.resetCss();//初始化css样式
+        this.resetCss(); //初始化css样式
         this.resetAll(); //初始化设置
         this.imgs.forEach((el) => {
             el.style.height = `${this.slideHeight}px`;
@@ -26,14 +28,20 @@
         this.autoStart = setInterval(() => {
             this.nextItem()
         }, timeOut);
+        //轮播间隔定时器
+
         for (let i = 0; i < this.navButtons.length; i++) {
             this.navButtons[i].onclick = this.navButtonClick.bind(this);
-        }
+        } //绑定导航圆点点击事件
+        //绑定触摸开始事件
+        this.slide.addEventListener('touchstart', this.touchstart.bind(this));
+        //绑定触摸结束事件
+        this.slide.addEventListener('touchend', this.touchend.bind(this));
     }
     //初始化样式
     Slide.prototype.resetCss = function () {
         let styleEle = document.createElement('style'); //创建style标签
-        let navLeft = this.slideWidth / 2 - this.imgs.length*9;
+        let navLeft = this.slideWidth / 2 - this.imgs.length * 9;
         let navTop = this.slideHeight / 40;
         styleEle.innerHTML += '.slide{position:relative;overflow:hidden;font-size:0;}'; //舒适化slide视窗的样式
         styleEle.innerHTML += '.slide .room{position:absolute;}'; //初始化room内容层的样式
@@ -63,8 +71,12 @@
         this.navButtons = this.slide.querySelectorAll('li'); //获取所有的li节点
         this.navButtons[0].style.background = '#333'; //初始化第一个圆点的颜色
     }
+    //切换下一个的函数
     Slide.prototype.nextItem = function () {
+        //清除过度定时器
         clearInterval(this.timer);
+        //清除间隔播放定时器
+        clearInterval(this.autoStart);
         this.timer = setInterval(() => {
             let left = parseInt(this.room.style.left);
             if (this.imgIndex < this.imgs.length) {
@@ -85,15 +97,50 @@
                     this.room.left = '0';
                     this.navButtons[this.imgIndex - 1].style.background = '#fff';
                     this.imgIndex = 1;
-                    this.navButtons[0].style.background = '#333';
+                    this.navButtons[this.imgIndex - 1].style.background = '#333';
                 }
             }
         }, 16.7);
+        this.autoStart = setInterval(() => {
+            this.nextItem()
+        }, this.timeOut);
+    }
 
+    Slide.prototype.lastItem = function () {
+        clearInterval(this.timer);
+        clearInterval(this.autoStart);
+        this.timer = setInterval(() => {
+            let left = parseInt(this.room.style.left);
+            if (this.imgIndex > 1) {
+                if (left < (-this.slideWidth * (this.imgIndex - 2))) {
+                    this.room.style.left = `${parseInt(this.room.style.left)-this.speed}px`;
+                } else {
+                    clearInterval(this.timer);
+                    this.room.style.left = `${-this.slideWidth*(this.imgIndex-2)}px`;
+                    this.navButtons[this.imgIndex - 1].style.background = '#fff';
+                    this.imgIndex--;
+                    this.navButtons[this.imgIndex - 1].style.background = '#333';
+                }
+            } else {
+                if (left > -this.slideWidth * (this.imgs.length - 1)) {
+                    this.room.style.left = `${parseInt(this.room.style.left)+(this.speed*2)}px`;
+                } else {
+                    clearInterval(this.timer);
+                    this.room.left = `${-this.slideWidth*this.imgs.length-1}px`;
+                    this.navButtons[this.imgIndex - 1].style.background = '#fff';
+                    this.imgIndex = this.imgs.length;
+                    this.navButtons[this.imgIndex - 1].style.background = '#333';
+                }
+            }
+        }, 16.7);
+        this.autoStart = setInterval(() => {
+            this.nextItem()
+        }, this.timeOut);
     }
     Slide.prototype.navButtonClick = function ({
         target
     }) {
+        clearInterval(this.autoStart);
         clearInterval(this.timer);
         this.timer = setInterval(() => {
             let left = parseInt(this.room.style.left);
@@ -119,11 +166,32 @@
                 return false;
             }
         }, 16.7);
+        this.autoStart = setInterval(() => {
+            this.nextItem()
+        }, this.timeOut);
     }
+    //导航圆点点击事件
     Slide.prototype.navButtonStyle = function (target) {
         target.style.background = '#333';
         this.navButtons[this.imgIndex - 1].style.background = '#fff';
         this.imgIndex = target.index;
+    }
+    //手指触摸起始触发函数
+    Slide.prototype.touchstart = function (e = window.event) {
+        this.startPoint = e.touches[0];
+    }
+    //手指触摸结束触发函数
+    Slide.prototype.touchend = function (e = window.event) {
+        let endPoint = e.changedTouches[0];
+        let x = endPoint.clientX - this.startPoint.clientX;
+        let y = endPoint.clientY - this.startPoint.clientY;
+        if (Math.abs(x) > this.d) {
+            if (x < 0) {
+                this.nextItem();
+            } else {
+                this.lastItem();
+            }
+        }
     }
     this.Slide = Slide;
 })();
--- slide5.js
+++ slide5.js
@@ -5,18 +5,21 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
 (function () {
     var Slide = function Slide() {
         var slideId = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'slide';
+        var timeOut = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 3000;
 
         var _this = this;
 
-        var timeOut = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 3000;
         var speed = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 0.1;
+        var d = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : 20;
 
+        this.d = d; //滑动灵敏度
+        this.timeOut = timeOut; //轮播间隔
         this.slide = document.querySelector('#slide'); //获取视窗层
         this.room = this.slide.querySelector('div'); //获取内容层
         this.imgs = [].concat(_toConsumableArray(this.slide.querySelectorAll('.slide-item'))); //轮播图元素组
         this.slideWidth = parseInt(getComputedStyle(this.slide).width); //视窗层高度
         this.slideHeight = parseInt(getComputedStyle(this.slide).height); //视窗层宽度
-
+        this.startPoint = null; //手指触摸的起点
         this.navButtons = null; //获取到所有的导航圆点
         this.timer = null; //定时器
         this.imgIndex = 1; //当前轮播图片index
@@ -32,9 +35,15 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
         this.autoStart = setInterval(function () {
             _this.nextItem();
         }, timeOut);
+        //轮播间隔定时器
+
         for (var i = 0; i < this.navButtons.length; i++) {
             this.navButtons[i].onclick = this.navButtonClick.bind(this);
-        }
+        } //绑定导航圆点点击事件
+        //绑定触摸开始事件
+        this.slide.addEventListener('touchstart', this.touchstart.bind(this));
+        //绑定触摸结束事件
+        this.slide.addEventListener('touchend', this.touchend.bind(this));
     };
     //初始化样式
     Slide.prototype.resetCss = function () {
@@ -69,10 +78,14 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
         this.navButtons = this.slide.querySelectorAll('li'); //获取所有的li节点
         this.navButtons[0].style.background = '#333'; //初始化第一个圆点的颜色
     };
+    //切换下一个的函数
     Slide.prototype.nextItem = function () {
         var _this2 = this;
 
+        //清除过度定时器
         clearInterval(this.timer);
+        //清除间隔播放定时器
+        clearInterval(this.autoStart);
         this.timer = setInterval(function () {
             var left = parseInt(_this2.room.style.left);
             if (_this2.imgIndex < _this2.imgs.length) {
@@ -93,44 +106,107 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
                     _this2.room.left = '0';
                     _this2.navButtons[_this2.imgIndex - 1].style.background = '#fff';
                     _this2.imgIndex = 1;
-                    _this2.navButtons[0].style.background = '#333';
+                    _this2.navButtons[_this2.imgIndex - 1].style.background = '#333';
                 }
             }
         }, 16.7);
+        this.autoStart = setInterval(function () {
+            _this2.nextItem();
+        }, this.timeOut);
+    };
+
+    Slide.prototype.lastItem = function () {
+        var _this3 = this;
+
+        clearInterval(this.timer);
+        clearInterval(this.autoStart);
+        this.timer = setInterval(function () {
+            var left = parseInt(_this3.room.style.left);
+            if (_this3.imgIndex > 1) {
+                if (left < -_this3.slideWidth * (_this3.imgIndex - 2)) {
+                    _this3.room.style.left = parseInt(_this3.room.style.left) - _this3.speed + 'px';
+                } else {
+                    clearInterval(_this3.timer);
+                    _this3.room.style.left = -_this3.slideWidth * (_this3.imgIndex - 2) + 'px';
+                    _this3.navButtons[_this3.imgIndex - 1].style.background = '#fff';
+                    _this3.imgIndex--;
+                    _this3.navButtons[_this3.imgIndex - 1].style.background = '#333';
+                }
+            } else {
+                if (left > -_this3.slideWidth * (_this3.imgs.length - 1)) {
+                    _this3.room.style.left = parseInt(_this3.room.style.left) + _this3.speed * 2 + 'px';
+                } else {
+                    clearInterval(_this3.timer);
+                    _this3.room.left = -_this3.slideWidth * _this3.imgs.length - 1 + 'px';
+                    _this3.navButtons[_this3.imgIndex - 1].style.background = '#fff';
+                    _this3.imgIndex = _this3.imgs.length;
+                    _this3.navButtons[_this3.imgIndex - 1].style.background = '#333';
+                }
+            }
+        }, 16.7);
+        this.autoStart = setInterval(function () {
+            _this3.nextItem();
+        }, this.timeOut);
     };
     Slide.prototype.navButtonClick = function (_ref) {
-        var _this3 = this;
+        var _this4 = this;
 
         var target = _ref.target;
 
+        clearInterval(this.autoStart);
         clearInterval(this.timer);
         this.timer = setInterval(function () {
-            var left = parseInt(_this3.room.style.left);
-            if (target.index > _this3.imgIndex) {
-                if (left > -_this3.slideWidth * (target.index - 1)) {
-                    _this3.room.style.left = parseInt(_this3.room.style.left) + _this3.speed * (target.index - _this3.imgIndex) + 'px';
+            var left = parseInt(_this4.room.style.left);
+            if (target.index > _this4.imgIndex) {
+                if (left > -_this4.slideWidth * (target.index - 1)) {
+                    _this4.room.style.left = parseInt(_this4.room.style.left) + _this4.speed * (target.index - _this4.imgIndex) + 'px';
                 } else {
-                    clearInterval(_this3.timer);
-                    _this3.room.style.left = -_this3.slideWidth * (target.index - 1) + 'px';
-                    _this3.navButtonStyle(target);
+                    clearInterval(_this4.timer);
+                    _this4.room.style.left = -_this4.slideWidth * (target.index - 1) + 'px';
+                    _this4.navButtonStyle(target);
                 }
-            } else if (target.index < _this3.imgIndex) {
-                if (left < -_this3.slideWidth * (target.index - 1)) {
-                    _this3.room.style.left = parseInt(_this3.room.style.left) - _this3.speed * (_this3.imgIndex - target.index) + 'px';
+            } else if (target.index < _this4.imgIndex) {
+                if (left < -_this4.slideWidth * (target.index - 1)) {
+                    _this4.room.style.left = parseInt(_this4.room.style.left) - _this4.speed * (_this4.imgIndex - target.index) + 'px';
                 } else {
-                    clearInterval(_this3.timer);
-                    _this3.room.style.left = -_this3.slideWidth * (target.index - 1) + 'px';
-                    _this3.navButtonStyle(target);
+                    clearInterval(_this4.timer);
+                    _this4.room.style.left = -_this4.slideWidth * (target.index - 1) + 'px';
+                    _this4.navButtonStyle(target);
                 }
             } else {
                 return false;
             }
         }, 16.7);
+        this.autoStart = setInterval(function () {
+            _this4.nextItem();
+        }, this.timeOut);
     };
+    //导航圆点点击事件
     Slide.prototype.navButtonStyle = function (target) {
         target.style.background = '#333';
         this.navButtons[this.imgIndex - 1].style.background = '#fff';
         this.imgIndex = target.index;
+    };
+    //手指触摸起始触发函数
+    Slide.prototype.touchstart = function () {
+        var e = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : window.event;
+
+        this.startPoint = e.touches[0];
+    };
+    //手指触摸结束触发函数
+    Slide.prototype.touchend = function () {
+        var e = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : window.event;
+
+        var endPoint = e.changedTouches[0];
+        var x = endPoint.clientX - this.startPoint.clientX;
+        var y = endPoint.clientY - this.startPoint.clientY;
+        if (Math.abs(x) > this.d) {
+            if (x < 0) {
+                this.nextItem();
+            } else {
+                this.lastItem();
+            }
+        }
     };
     this.Slide = Slide;
 })();
